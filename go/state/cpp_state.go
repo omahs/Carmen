15b1da71e72c2e2902a7d7473c5f0bbe596e7c27
Jan Kalina <jan.kalina@fantom.foundation>
2023-10-25 17:23:04 +0200

    Experiment

--- go/state/cpp_state.go
+++ go/state/cpp_state.go
@@ -2,13 +2,6 @@ package state
 
 //go:generate sh ../lib/build_libcarmen.sh
 
-/*
-#cgo CFLAGS: -I${SRCDIR}/../../cpp
-#cgo LDFLAGS: -L${SRCDIR}/../lib -lcarmen
-#cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/../lib
-#include <stdlib.h>
-#include "state/c_state.h"
-*/
 import "C"
 import (
 	"encoding/binary"
---
+++ go/state/supportcpp/support.go
@@ -0,0 +1,9 @@
+package supportcpp
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/../../cpp
+#cgo LDFLAGS: -L${SRCDIR}/../lib -lcarmen
+#cgo LDFLAGS: -Wl,-rpath,${SRCDIR}/../lib
+#include <stdlib.h>
+#include "state/c_state.h"
+*/
