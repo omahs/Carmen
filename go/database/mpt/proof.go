c7a03ec974464bf4c5214fe7fce294bc4f50af5b
Kamil Jezek <kamil.jezek@gmail.com>
2024-05-23 17:28:45 +0200

    address review comments

--- go/database/mpt/proof.go
+++ go/database/mpt/proof.go
@@ -6,66 +6,120 @@ import (
 
 //go:generate mockgen -source proof.go -destination proof_mocks.go -package witness
 
-// RlpEncodedNode is an RLP encoded MPT node.
-type RlpEncodedNode []byte
+// rlpEncodedNode is an RLP encoded MPT node.
+type rlpEncodedNode []byte
 
 // proofDb is a database of RLP encoded MPT nodes and their hashes that represent witness proofs.
-type proofDb map[common.Hash]RlpEncodedNode
+type proofDb map[common.Hash]rlpEncodedNode
 
 // WitnessProof represents a witness proof.
 // It contains a database of MPT nodes and their hashes.
 // The proof may be verified by iterating the proof database from the
 // root hash to a terminal entry according to a Path
 // representing either an account address or a storage key.
-// The proof is valid if it can be iterated to an expected terminal entry.
+// If the proof contains an account or a storage slot for the input address or key,
+// the proof is representing this account or storage.
 type WitnessProof proofDb
 
-// Merge merges the input witness proof into the current witness proof.
-func (p *WitnessProof) Merge(other *WitnessProof) {
-	for k, v := range *other {
-		(*p)[k] = v
+// CreateWitnessProof creates a witness proof for the input account address
+// and possibly storage slots of the same account under the input storage keys.
+// If the proof cannot be created because either the address or a key does not exist,
+// it returns false.
+// It may return an error when it occurs in the underlying database.
+func CreateWitnessProof(nodeSource NodeSource, root *NodeReference, address common.Address, keys ...common.Key) (WitnessProof, bool, error) {
+	return nil, false, nil
+}
+
+// Add merges the input witness proof into the current witness proof.
+func (p WitnessProof) Add(other WitnessProof) {
+	for k, v := range other {
+		p[k] = v
 	}
 }
 
-// ExtractAccountProof extracts a sub-proof from the current witness proof.
-// It returns a copy that contains only proof for the input address and storage keys.
-// If the input address or keys are not found in the current proof, it returns an empty proof.
-func (p *WitnessProof) ExtractAccountProof(nodeSource NodeSource, root common.Hash, address common.Address, keys ...common.Key) *WitnessProof {
-	return &WitnessProof{}
+// ExtractAccount extracts an account and storage from the current witness proof.
+// It returns a copy that contains only a proof for the input address and storage keys.
+// If the input address or keys are not found in the proof, it returns a proof that
+// contains only partial proof consisting only of elements that could be reached
+// from the input address and keys in the current proof.
+// If the proof cannot be fully extracted, this method returns false.
+func (p WitnessProof) ExtractAccount(root common.Hash, address common.Address, keys ...common.Key) (WitnessProof, bool) {
+	return WitnessProof{}, false
 }
 
-// ExtractStorageProof extracts a sub-proof from the current witness proof.
-// It returns a copy that contains only proof for the storage key.
-// If the key is not found in the current proof, it returns an empty proof.
-func (p *WitnessProof) ExtractStorageProof(nodeSource NodeSource, storageRoot common.Hash, address common.Address, key common.Key) *WitnessProof {
-	return &WitnessProof{}
+// IsValid checks that the proof has all valid entries.
+// It means, RLPs are valid encodings and their hashes match the key in the map.
+func (p WitnessProof) IsValid() bool {
+	return false
 }
 
-// ProveAccount verifies the witness proof against the input root hash and address.
-// If the witness proof represents the input account for the input address, it returns the account node.
-// If the proof is invalid or does not represent the account, it returns nil.
-func (p *WitnessProof) ProveAccount(nodeSource NodeSource, root common.Hash, address common.Address) *AccountNode {
-	path := AddressToNibblePath(address, nodeSource)
-	node := p.provePath(root, path)
+// GetAccountInfo extracts an account info from the witness proof for the input root hash and the address.
+// If the witness proof contains an account for the input address, it returns its information.
+// If the proof does not contain an account, it returns false.
+// The method may return an error if the proof is invalid.
+func (p WitnessProof) GetAccountInfo(root common.Hash, address common.Address) (AccountInfo, bool, error) {
+	path := AddressToNibblePath(address, nil) // TODO hash path without the node source
+	node := p.getNext(root, path)
 	if node == nil {
-		return nil
+		return AccountInfo{}, false, nil
 	}
-	return node.(*AccountNode)
+	return node.(*AccountNode).info, true, nil
 }
 
-// ProveStorage verifies the witness proof against the input storage root hash, and storage key.
-// If the witness proof represents the input storage slot for the input key, it returns the value node.
-// If the proof is invalid or does not represent the storage slot, it returns nil.
-func (p *WitnessProof) ProveStorage(nodeSource NodeSource, storageRoot common.Hash, key common.Key) *ValueNode {
-	path := KeyToNibblePath(key, nodeSource)
-	node := p.provePath(storageRoot, path)
+// GetBalance extracts a balance from the witness proof for the input root hash and the address.
+// If the witness proof contains an account for the input address, it returns its balance.
+// If the proof does not contain an account, it returns false.
+// The method may return an error if the proof is invalid.
+func (p WitnessProof) GetBalance(root common.Hash, address common.Address) (common.Balance, bool, error) {
+	return common.Balance{}, false, nil
+}
+
+// GetNonce extracts a nonce from the witness proof for the input root hash and the address.
+// If the witness proof contains an account for the input address, it returns its nonce.
+// If the proof does not contain an account, it returns false.
+// The method may return an error if the proof is invalid.
+func (p WitnessProof) GetNonce(root common.Hash, address common.Address) (common.Nonce, bool, error) {
+	return common.Nonce{}, false, nil
+}
+
+// GetCodeHash extracts a code hash from the witness proof for the input root hash and the address.
+// If the witness proof contains an account for the input address, it returns its code hash.
+// If the proof does not contain an account, it returns false.
+// The method may return an error if the proof is invalid.
+func (p WitnessProof) GetCodeHash(root common.Hash, address common.Address) (common.Hash, bool, error) {
+	return common.Hash{}, false, nil
+}
+
+// GetState extracts a storage slot from the witness proof for the input root hash, account address and the storage key.
+// If the witness proof contains an input storage slot for the input key, it returns its value.
+// If the proof does not contain a slot, it returns false.
+// The method may return an error if the proof is invalid.
+func (p WitnessProof) GetState(root common.Hash, address common.Address, key common.Key) (common.Value, bool, error) {
+	path := KeyToNibblePath(key, nil) // TODO hash path without the node source
+	node := p.getNext(root, path)
 	if node == nil {
-		return nil
+		return common.Value{}, false, nil
 	}
-	return node.(*ValueNode)
+	return node.(*ValueNode).value, true, nil
 }
 
-func (p *WitnessProof) provePath(root common.Hash, path []Nibble) Node {
+// AllStatesZero checks that all storage slots are empty for the input root hash, account address and the storage key range.
+// If the witness proof contains all empty slots for the input key range, it returns true.
+// An empty slot is a slot that contains a zero value, or does not exist at all.
+// If the proof contains a slot, it returns false.
+func (p WitnessProof) AllStatesZero(root common.Hash, address common.Address, from, to common.Key) (bool, error) {
+	return false, nil
+}
+
+// AllAddressesEmpty checks that all accounts are empty for the input root hash and the address range.
+// If the witness proof contains all empty accounts for the input address range, it returns true.
+// An empty account is an account that contains a zero balance, nonce, and code hash.
+// If the proof contains an account, it returns false.
+func (p WitnessProof) AllAddressesEmpty(root common.Hash, from, to common.Address) (bool, error) {
+	return false, nil
+}
+
+func (p WitnessProof) getNext(root common.Hash, path []Nibble) Node {
 	// TODO draft of the algorithm
 
 	//rlp, exists := (*p)[root]
@@ -81,21 +135,15 @@ func (p *WitnessProof) provePath(root common.Hash, path []Nibble) Node {
 	//	return node
 	//}
 	//
-	//return p.provePath(nextRoot, subPath)
+	//return p.getNext(nextRoot, subPath)
 
 	return nil
-}
-
-// CreateWitnessProof creates a witness proof for the input account address
-// and possibly storage slots of the same account under the input storage keys.
-func CreateWitnessProof(nodeSource NodeSource, root *NodeReference, address common.Address, keys ...common.Key) (*WitnessProof, error) {
-	return nil, nil
 }
 
 // proofExtractionVisitor is a visitor that visits MPT nodes and creates a witness proof.
 // It hashes and encodes the nodes and stores them into the proof database.
 type proofExtractionVisitor struct {
-	proof      *WitnessProof
+	proof      WitnessProof
 	nodeSource NodeSource
 	err        error
 }
@@ -103,14 +151,14 @@ type proofExtractionVisitor struct {
 // Visit computes RLP and hash of the visited node and puts it to the proof.
 func (p *proofExtractionVisitor) Visit(node Node, _ NodeInfo) VisitResponse {
 	data := make([]byte, 0, 1024)
-	rlp, err := encode(node, p.nodeSource, data)
+	rlp, err := encodeToRlp(node, p.nodeSource, data)
 	if err != nil {
 		p.err = err
 		return VisitResponseAbort
 	}
 	hash := common.Keccak256(rlp)
 
-	(*p.proof)[hash] = rlp
+	p.proof[hash] = rlp
 
 	return VisitResponseContinue
 }
